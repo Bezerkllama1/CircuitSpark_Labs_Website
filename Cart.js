c5a50b1dd3e54d4ffa753d7d71120a8d4c94c721
Bezerkllama1 <millerlaurence413@icloud.com>
2025-04-22 20:02:04 -0400

    did some mild and some extreme editing
    added things and modified the cart page. is not final.

--- Cart.js
+++ Cart.js
@@ -1,16 +1,8 @@
-document.querySelectorAll('.add-to-cart').forEach(button => {
-    button.addEventListener('click', function() {
-        const productElement = this.parentElement;
-        const productId = '1'; // You can set a unique ID for each product
-        const productName = productElement.querySelector('h1').textContent.trim(); // Get the product name
+//document.querySelectorAll('.checkout').forEach(button => {
+  //  button.addEventListener('click', function() {
         
-        const cartItem = document.createElement('li');
-        cartItem.innerHTML = `${productName} <button class="remove-from-cart">Remove</button>`;
-        cartItem.setAttribute('data-productid', productId);
-        
-        document.getElementById('cart').appendChild(cartItem);
-    });
-});
+    //});
+//});
 
 document.getElementById('cart').addEventListener('click', function(e) {
     if (e.target.classList.contains('remove-from-cart')) {
